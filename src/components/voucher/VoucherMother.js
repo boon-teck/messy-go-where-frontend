dc86ab233a6a6fdbd0ef565046c6c18cda37b280
JosiahPhua <josiahphuajy@gmail.com>
2021-06-24 21:13:27 +0800

    checkpoint

--- src/components/voucher/VoucherMother.js
+++ src/components/voucher/VoucherMother.js
@@ -4,7 +4,12 @@ import axios from 'axios';
 
 function VoucherMother({auth, setAuth, user}) {
     const [vouchers, setVouchers] = useState({})
-    const [buy, setBuy] = useState(null)
+    const [vACount, setVACount] = useState("")
+    const [vBCount, setVBCount] = useState("")
+    const [vCCount, setVCCount] = useState("")
+    const [vDCount, setVDCount] = useState("")
+    let allCount = [vACount, vBCount, vCCount, vDCount]
+
 
     useEffect(()=>{
 
@@ -18,13 +23,16 @@ function VoucherMother({auth, setAuth, user}) {
                 })
 
                 setVouchers(data.availableVouchers.voucherTemplate)
-
+                setVACount(data.voucherACount)
+                setVBCount(data.voucherBCount)
+                setVCCount(data.voucherCCount)
+                setVDCount(data.voucherDCount)
+                console.log(data)
             } catch (error) {
                 console.log(error)
             }
         }
         getVouchers()
-
         
     },[])
 
@@ -38,15 +46,28 @@ function VoucherMother({auth, setAuth, user}) {
                 headers: {
                     authorization: `Bearer ${localStorage.token}`
                 }
-        })
-
-
+            })
         } catch (error) {
             console.log(error)
         }
     }
 
-    console.log(vouchers)
+    const redeemVoucher = async (id) =>{
+        let num = {
+            "num" : id
+        } 
+        
+        try {
+            let {data} = await axios.post(`/api/vouchers/redeem`, num, {
+                headers: {
+                    authorization: `Bearer ${localStorage.token}`
+                }
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <Container>
@@ -60,9 +81,11 @@ function VoucherMother({auth, setAuth, user}) {
                                 <Card.Body>
                                     <Card.Title>{el.name}</Card.Title>
                                     <Card.Text>
-                                        This voucher cost {el.pointsCost}points
+                                        This voucher cost {el.pointsCost}points<br />
+                                       You have {allCount[id]} number of voucher(s).
                                     </Card.Text>
-                                    <Button onClick={()=>buyVoucher(id)} variant="primary">Exchange</Button>
+                                    <Button onClick={()=>buyVoucher(id)} variant="primary">Buy</Button>
+                                    <Button onClick={()=>redeemVoucher(id)} variant="primary">Use</Button>
                                 </Card.Body>
                             </Card>
 
